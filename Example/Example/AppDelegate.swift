2f1cffdc7f58dbf92a40616553d04e1c22f449f6
Danil Gontovnik <gontovnik.danil@gmail.com>
2016-06-27 22:23:42 +0100

    Empty example project

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Danil Gontovnik on 27/06/2016.
+//  Copyright © 2016 Kaizen Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+    
+    // MARK: - Vars
+
+    var window: UIWindow?
+
+}
+
+// MARK: - UIApplicationDelegate
+
+extension AppDelegate: UIApplicationDelegate {
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+    
+}
